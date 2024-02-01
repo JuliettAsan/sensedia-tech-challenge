52c6d574836dbd4ac344676cf52a5dcda38e02bd
juliettAsan <juliethsanchez9595@gmail.com>
2024-01-31 21:39:46 -0500

    Create folder architecture

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const path = require("path");
+
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "main/styles")],
+    prependData: `@import "_main.scss";`,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+};
+
+module.exports = nextConfig;
